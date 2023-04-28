bafaef00f4e679c40738a68e6938c97d319dd3f9
SakaiTaka23 <tt3.sakai@gmail.com>
2023-04-28 13:59:50 +0900

    ミドルウェアのコードを整理した

--- gin/base/router/router.go
+++ gin/base/router/router.go
@@ -5,11 +5,22 @@ import (
 	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
+	"log"
 	"time"
 )
 
+type middleware struct {
+	l *zap.Logger
+}
+
 func SetRouter(r *gin.Engine) *gin.Engine {
-	useLogger(r)
+	logger, _ := zap.NewProduction()
+	r.Use(ginzap.Ginzap(logger, time.RFC3339, true))
+	r.Use(ginzap.RecoveryWithZap(logger, true))
+
+	midd := initMiddleware()
+	r.Use(midd.useLogger())
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
@@ -21,9 +32,13 @@ func SetRouter(r *gin.Engine) *gin.Engine {
 	return r
 }
 
-func useLogger(r *gin.Engine) *gin.Engine {
-	logger, _ := zap.NewProduction()
-	r.Use(ginzap.Ginzap(logger, time.RFC3339, true))
-	r.Use(ginzap.RecoveryWithZap(logger, true))
-	return r
+func initMiddleware() *middleware {
+	return &middleware{}
+}
+
+func (m *middleware) useLogger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		log.Print("request start: ", c.Request.URL)
+		c.Next()
+	}
 }
