e6f08c2fa2d80cb26bb62d7e904e38c9b59b8a6f
SakaiTaka23 <tt3.sakai@gmail.com>
2023-05-13 22:27:18 +0900

    テーブルテストを追加してみた

--- gomock/usecase/user_usecase_test.go
+++ gomock/usecase/user_usecase_test.go
@@ -1,6 +1,7 @@
 package usecase
 
 import (
+	"fmt"
 	"github.com/golang/mock/gomock"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
@@ -37,4 +38,59 @@ func Test_userUsecase_GetUser(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, findID, user.ID)
+}
+
+func Test_userUsecase_CreateUser1(t *testing.T) {
+	type fields struct {
+		userRepo *mock.MockUserRepo
+	}
+	type args struct {
+		name string
+	}
+	createdID := uuid.New().String()
+	tests := []struct {
+		name    string
+		prepare func(f *fields)
+		args    args
+		want    string
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "success",
+			prepare: func(f *fields) {
+				f.userRepo.EXPECT().Create("John Doe").Return(createdID, nil)
+			},
+			args:    args{name: "John Doe"},
+			want:    createdID,
+			wantErr: assert.NoError,
+		},
+		{
+			name:    "name must not be empty",
+			prepare: nil,
+			args:    args{name: ""},
+			want:    "",
+			wantErr: assert.Error,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			f := fields{userRepo: mock.NewMockUserRepo(ctrl)}
+			if tt.prepare != nil {
+				tt.prepare(&f)
+			}
+
+			u := &userUsecase{
+				userRepo: f.userRepo,
+			}
+
+			got, err := u.CreateUser(tt.args.name)
+			if !tt.wantErr(t, err, fmt.Sprintf("CreateUser(%v)", tt.args.name)) {
+				return
+			}
+			assert.Equalf(t, tt.want, got, "CreateUser(%v)", tt.args.name)
+		})
+	}
 }
